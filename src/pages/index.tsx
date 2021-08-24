10343fb8b52e3ece3d86fc5e8174b1c8ca564b3e
Igor Cunha <igorcesarcunha@gmail.com>
2021-08-24 19:23:17 -0400

    home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { Flex, Text, Image } from '@chakra-ui/react';
+
+export default function Home(): JSX.Element {
+  return (
+    <Flex
+      maxW={1120}
+      height="100%"
+      flexDirection="column"
+      align="center"
+      justify="space-around"
+      p={4}
+    >
+      <Flex fontSize="3xl" color="white" fontWeight="600">
+        <p>
+          Hello,
+          <br /> my name is Igor Cunha,
+          <br />I am a{' '}
+          <Text as="span" color="cPurple.500">
+            Full Stack Web Developer
+          </Text>
+        </p>
+      </Flex>
+
+      <Image src="images/coding.svg" alt="coding" zIndex={2} bottom="60px" />
+    </Flex>
+  );
+}
